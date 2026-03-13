5e9847c1be253357cf9c4efecfd5d92a66cebd0b
copdowncow <parampapam755@gmail.com>
2026-03-14 00:12:28 +0500

    Update CartPage.tsx

--- client/src/pages/CartPage.tsx
+++ client/src/pages/CartPage.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { Trash2, Plus, Minus, ArrowRight, ShoppingBag } from 'lucide-react'
 import { useCart } from '../hooks/useCart'
