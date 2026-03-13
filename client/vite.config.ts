26d49684c20aae727ea14bcf9d8931d4d2ff8e62
copdowncow <parampapam755@gmail.com>
2026-03-14 01:36:39 +0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,15 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
+  root: resolve(__dirname),
   plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
     proxy: {
