3c479c60602884f83b7af0e2505fd8d30625824b
copdowncow <parampapam755@gmail.com>
2026-03-14 01:39:39 +0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,10 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  root: resolve(__dirname),
   plugins: [react()],
   build: {
-    outDir: resolve(__dirname, 'dist'),
+    outDir: '../client/dist',
     emptyOutDir: true,
   },
   server: {
